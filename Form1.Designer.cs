e3ca749dcdeee0216c48f41ee2aac9b8dcb40ad8
toppingj <155089757+toppingj@users.noreply.github.com>
2023-12-28 09:59:54 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace FinalExam_topping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.multiplyButton = new System.Windows.Forms.Button();
+            this.compareButton = new System.Windows.Forms.Button();
+            this.forLoopButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.methodWithOutputButton = new System.Windows.Forms.Button();
+            this.methodWithReturnValueButton = new System.Windows.Forms.Button();
+            this.arrayButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 128);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(190, 108);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(190, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // multiplyButton
+            // 
+            this.multiplyButton.Location = new System.Drawing.Point(233, 12);
+            this.multiplyButton.Name = "multiplyButton";
+            this.multiplyButton.Size = new System.Drawing.Size(75, 23);
+            this.multiplyButton.TabIndex = 3;
+            this.multiplyButton.Text = "Multiply";
+            this.multiplyButton.UseVisualStyleBackColor = true;
+            this.multiplyButton.Click += new System.EventHandler(this.multiplyButton_Click);
+            // 
+            // compareButton
+            // 
+            this.compareButton.Location = new System.Drawing.Point(233, 41);
+            this.compareButton.Name = "compareButton";
+            this.compareButton.Size = new System.Drawing.Size(75, 23);
+            this.compareButton.TabIndex = 4;
+            this.compareButton.Text = "Compare";
+            this.compareButton.UseVisualStyleBackColor = true;
+            this.compareButton.Click += new System.EventHandler(this.compareButton_Click);
+            // 
+            // forLoopButton
+            // 
+            this.forLoopButton.Location = new System.Drawing.Point(233, 70);
+            this.forLoopButton.Name = "forLoopButton";
+            this.forLoopButton.Size = new System.Drawing.Size(75, 23);
+            this.forLoopButton.TabIndex = 5;
+            this.forLoopButton.Text = "Loop";
+            this.forLoopButton.UseVisualStyleBackColor = true;
+            this.forLoopButton.Click += new System.EventHandler(this.forLoopButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(233, 99);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 6;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(233, 128);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // methodWithOutputButton
+            // 
+            this.methodWithOutputButton.Location = new System.Drawing.Point(233, 184);
+            this.methodWithOutputButton.Name = "methodWithOutputButton";
+            this.methodWithOutputButton.Size = new System.Drawing.Size(75, 23);
+            this.methodWithOutputButton.TabIndex = 8;
+            this.methodWithOutputButton.Text = "Output";
+            this.methodWithOutputButton.UseVisualStyleBackColor = true;
+            this.methodWithOutputButton.Click += new System.EventHandler(this.methodWithOutputButton_Click);
+            // 
+            // methodWithReturnValueButton
+            // 
+            this.methodWithReturnValueButton.Location = new System.Drawing.Point(233, 155);
+            this.methodWithReturnValueButton.Name = "methodWithReturnValueButton";
+            this.methodWithReturnValueButton.Size = new System.Drawing.Size(75, 23);
+            this.methodWithReturnValueButton.TabIndex = 9;
+            this.methodWithReturnValueButton.Text = "Return";
+            this.methodWithReturnValueButton.UseVisualStyleBackColor = true;
+            this.methodWithReturnValueButton.Click += new System.EventHandler(this.methodWithReturnValueButton_Click);
+            // 
+            // arrayButton
+            // 
+            this.arrayButton.Location = new System.Drawing.Point(233, 213);
+            this.arrayButton.Name = "arrayButton";
+            this.arrayButton.Size = new System.Drawing.Size(75, 23);
+            this.arrayButton.TabIndex = 10;
+            this.arrayButton.Text = "Array";
+            this.arrayButton.UseVisualStyleBackColor = true;
+            this.arrayButton.Click += new System.EventHandler(this.arrayButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Enter Value 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Enter Value 2:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 271);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.arrayButton);
+            this.Controls.Add(this.methodWithReturnValueButton);
+            this.Controls.Add(this.methodWithOutputButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.forLoopButton);
+            this.Controls.Add(this.compareButton);
+            this.Controls.Add(this.multiplyButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Final Exam by Jakob Topping";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button multiplyButton;
+        private System.Windows.Forms.Button compareButton;
+        private System.Windows.Forms.Button forLoopButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button methodWithOutputButton;
+        private System.Windows.Forms.Button methodWithReturnValueButton;
+        private System.Windows.Forms.Button arrayButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace FinalExam_topping
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void multiplyButton_Click(object sender, EventArgs e)
+        {
+            // Button 3 - Multiply
+            if (double.TryParse(textBox1.Text, out double num1) && double.TryParse(textBox2.Text, out double num2))
+            {
+                double result = num1 * num2;
+                listBox1.Items.Add($"Multiplication Result: {result}");
+            }
+            else
+            {
+                listBox1.Items.Add("Invalid input. Please enter valid numbers.");
+            }
+        }
+
+        private void compareButton_Click(object sender, EventArgs e)
+        {
+            // Button 4 - Compare
+            if (double.TryParse(textBox1.Text, out double num1) && double.TryParse(textBox2.Text, out double num2))
+            {
+                if (num1 > num2)
+                    listBox1.Items.Add($"{num1} is bigger than {num2}");
+                else if (num1 < num2)
+                    listBox1.Items.Add($"{num2} is bigger than {num1}");
+                else
+                    listBox1.Items.Add($"{num1} and {num2} are equal");
+            }
+            else
+            {
+                listBox1.Items.Add("Invalid input. Please enter valid numbers.");
+            }
+        }
+
+        private void forLoopButton_Click(object sender, EventArgs e)
+        {
+            // Button 5 - For Loop
+            listBox1.Items.Add("Numbers 1 to 10:");
+            for (int i = 1; i <= 10; i++)
+            {
+                listBox1.Items.Add(i);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Button 6 - Clear
+            listBox1.Items.Clear();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Button 7 - Exit
+            Close();
+        }
+
+        private void methodWithReturnValueButton_Click(object sender, EventArgs e)
+        {
+            // Button 8 - Method with Return Value
+            string message = GetMessageFromMethod();
+            listBox1.Items.Add(message);
+        }
+
+        private string GetMessageFromMethod()
+        {
+            // Method for Button 8
+            return "This button calls a method in step #8.";
+        }
+
+        private void methodWithOutputButton_Click(object sender, EventArgs e)
+        {
+            // Button 9 - Method with Output
+            DisplayMessageFromMethod();
+        }
+
+        private void DisplayMessageFromMethod()
+        {
+            // Method for Button 9
+            listBox1.Items.Add("The method #9 has been called.");
+        }
+
+        private void arrayButton_Click(object sender, EventArgs e)
+        {
+            // Button 10 - Array
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            listBox1.Items.Add("Array Values:");
+            foreach (int value in array)
+            {
+                listBox1.Items.Add(value);
+            }
+        }
+    }
+}
